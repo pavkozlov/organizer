5d8387dee8cc72b212e3c0ec48fbe4faa10c86cc
pkozlov <it.pavelkozlov@gmail.com>
2020-03-05 21:00:58 +0300

    models refactored

--- models/todo.go
+++ models/todo.go
@@ -1,6 +1,10 @@
 package models
 
+import "time"
+
 type Todo struct {
 	ID    uint   `json:"id"`
 	Title string `form:"title" json:"title" binding:"required"`
+	Complete bool `default:"false" json:"complete"`
+	CreatedAt time.Time `gorm:"type:timestamp" json:"created"`
 }
