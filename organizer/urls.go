ee065657228dacad1006a45ba37f10a77f7e513a
pkozlov <it.pavelkozlov@gmail.com>
2020-03-26 13:13:22 +0300

    refactor

--- organizer/urls.go
+++ organizer/urls.go
@@ -1,11 +1,13 @@
 package organizer
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(SetCors())
+	r.Use(cors.Default())
 	return r
 }
